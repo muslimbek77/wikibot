2cbe8312b6d9b27b1d4eaab9829ad7930ff700e0
Muslimbek <muslimjonbekali@mail.ru>
2022-02-10 05:44:09 +0500

    logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from telegram.bot import Bot
 from telegram.user import User
 import settings
 from telegram.ext import Updater,Dispatcher,CommandHandler,CallbackContext
+import requests
 
 
 bot =Bot(token=settings.TELEGRAM_TOKEN)
@@ -13,9 +14,31 @@ updater=Updater(token=settings.TELEGRAM_TOKEN)
 def start(update:Update,context:CallbackContext):
     update.message.reply_text("salom")
     context.bot.send_message(chat_id=update.message.chat_id,text='yana bir bor salom')
+def search(update:Update,context:CallbackContext):
+    args=context.args
+    search_text=' '.join(args)
+    response=requests.get('https://en.wikipedia.org/w/api.php',{
+        'action':'opensearch',
+        'search': search_text,
+        'limit':1,
+        'namespace':0,
+        'format':'json',
+    })
+    result=response.json()
+    if len(args)==0:        
+        update.message.reply_text("Hech bo'lmasa nimadir kiriting")
+
+
+    elif len(result[3]):
+        update.message.reply_text("sizning so'rovvingiz bo'yicha havola "+ result[3][0])
+    else:
+        update.message.reply_text("sizning so'rovvingiz bo'yicha hech nima topilmadi")
+    
 
 dispatcher=updater.dispatcher
 dispatcher.add_handler(CommandHandler('start',start))
+dispatcher.add_handler(CommandHandler('search',search))
+
 
 updater.start_polling()
 updater.idle()
